7cd3e16dc9647e5a2bd1eebd0f99974bdf6b4784
Lars-Erik Aabech <lea@markedspartner.no>
2020-10-19 20:37:37 +0200

    Added program and empty test

---
+++ src/test/java/The_program.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class The_program {
+    @Test
+    public void does_something(){
+        fail("Program doesn't do anything yet.");
+    }
+}
