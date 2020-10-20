785f6fc9f3b31814de22474f6ff4d74bba045a7b
Lars-Erik Aabech <lea@markedspartner.no>
2020-10-20 22:52:34 +0200

    Added first draft of domain model

---
+++ src/main/java/BinaryExpression.java
@@ -0,0 +1,11 @@
+public class BinaryExpression {
+    public final int left;
+    public final int right;
+    public final char operator;
+
+    public BinaryExpression(int left, int right, char operator) {
+        this.left = left;
+        this.right = right;
+        this.operator = operator;
+    }
+}
